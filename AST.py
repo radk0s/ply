28d90c30e2273554e01fd8a40b60fccb61825989
Dawid Szmigielski <althardaine@gmail.com>
2014-01-25 21:26:18 +0100

    Changed AST and Cparser to distinct different classes and create tree structure.

--- AST.py
+++ AST.py
@@ -1,9 +1,312 @@
 
 class Node(object):
+    def __init__(self, first, second, third):
+        self.first = first
+        self.second = second
+        self.third = third
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i) + self.third.toString(i)
 
-    def __str__(self):
-        return self.printTree()
+class Declarations(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
 
+class Inits(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Inits1(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Instructions(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Instuctions1(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Instruction(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Expression2(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class ExprList(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class CompoundInst(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Condition(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class ExpressionList(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class ExpressionList1(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class FunDefs(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class ArgListOrEmpty(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class ArgList(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        return self.first.toString(i) + self.second.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class ArgList1(object):
+    def __init__(self, first):
+        self.first = first
+    def toString(self, i):
+        return self.first.toString(i)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Arg(object):
+    def __init__(self, TYPE, ID):
+        self.TYPE = TYPE
+        self.ID = ID
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + 'ARG ' + self.ID + '\n'
+    def setParent(self, parent):
+        self.parent = parent
+
+class Fundef(object):
+    def __init__(self, TYPE, ID, first, second):
+        self.TYPE = TYPE
+        self.ID = ID
+        self.first = first
+        self.second = second
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        #return pal + self.TYPE + '\n' + pal + self.ID + '\n' + self.first.toString(i+1) + self.second.toString(i+1)
+        return pal + 'FUNDEF' + '\n' + pal + '| ' + self.ID + '\n' + '| ' + 'RET ' + self.TYPE + '\n' + self.first.toString(i+1) + self.second.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Expression(object):
+    def __init__(self, expr1, sign, expr2):
+        self.expr1 = expr1
+        self.sign = sign
+        self.expr2 = expr2
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + self.sign + '\n' +  self.expr1.toString(i+1) + self.expr2.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class MathExpression(object):
+    def __init__(self, expr1, sign, expr2):
+        self.expr1 = expr1
+        self.sign = sign
+        self.expr2 = expr2
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + self.sign + '\n' +  self.expr1.toString(i+1) + self.expr2.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Expression1(object):
+    def __init__(self, ID, second):
+        self.ID = ID
+        self.second = second
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + self.ID.upper() + '\n' + self.second.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Funcall(object):
+    def __init__(self, ID, second):
+        self.ID = ID
+        self.second = second
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + 'FUNCALL\n' + pal + '| ' + self.ID + '\n' + self.second.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Repeat(object):
+    def __init__(self, REPEAT, inst, UNTIL, cond):
+        self.REAPEAT = REPEAT
+        self.inst = inst
+        self.UNTIL = UNTIL
+        self.cond = cond
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + self.REAPEAT + '\n' + self.inst.toString(i+1) + pal + self.UNTIL + '\n' + self.cond.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class While(object):
+    def __init__(self, WHILE, condition, instruction):
+        self.WHILE = WHILE
+        self.condition = condition
+        self.instruction = instruction
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + self.WHILE.upper() + '\n' + self.condition.toString(i+1) + self.instruction.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Choice(object):
+    def __init__(self, IF, cond, inst, ELSE, inst2):
+        self.IF = IF
+        self.cond = cond
+        self.inst = inst
+        self.ELSE = ELSE
+        self.inst2 = inst2
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + self.IF.upper() + '\n' + self.cond.toString(i+1) + self.inst.toString(i+1) + pal + self.ELSE.upper() + '\n' + self.inst2.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Choice2(object):
+    def __init__(self, IF, cond, instr):
+        self.IF = IF
+        self.cond = cond
+        self.instr = instr
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + self.IF.upper() + '\n' + self.cond.toString(i+1) + self.instr.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Assignment(object):
+    def __init__(self, ID, expr):
+        self.ID = ID
+        self.expr = expr
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + '=' + '\n' + pal + '| ' + self.ID + '\n' + self.expr.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Declaration(object):
+    def __init__(self, inits):
+        self.inits = inits
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return pal + 'DECL' + '\n' + self.inits.toString(i+1)
+    def setParent(self, parent):
+        self.parent = parent
+
+class Empty(object):
+    def __init__(self):
+        self.name = ""
+    def toString(self, i):
+        pal = ""
+        for x in range(0, i):
+            pal += '| '
+        return self.name
+    def setParent(self, parent):
+        self.parent = parent
 
 class Const(Node):
     def __init__(self, name):
@@ -13,6 +316,8 @@ class Const(Node):
         for x in range(0, i):
             pal += '| '
         return pal + self.name + '\n'
+    def setParent(self, parent):
+        self.parent = parent
 
 
 class Integer(Const):
@@ -31,160 +336,14 @@ class Variable(Node):
     def __init__(self, name):
         self.name = name
 
+
+
 class BinExpr(Node):
     def __init__(self, o1, op, o2):
         self.o1 = o1
         self.op = op
         self.o2 = o2
 
-class One(object):
-    def __init__(self, first):
-        self.first = first
-    def toString(self, i):
-        return self.first.toString(i)
 
-class Two(object):
-    def __init__(self, first, second):
-        self.first = first
-        self.second = second
-    def toString(self, i):
-        return self.first.toString(i) + self.second.toString(i)
-
-class Three(object):
-    def __init__(self, first, second, third):
-        self.first = first
-        self.second = second
-        self.third = third
-    def toString(self, i):
-        return self.first.toString(i) + self.second.toString(i) + self.third.toString(i)
-
-class Arg(object):
-    def __init__(self, TYPE, ID):
-        self.TYPE = TYPE
-        self.ID = ID
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + 'ARG ' + self.ID + '\n'
-
-class Fundef(object):
-    def __init__(self, TYPE, ID, first, second):
-        self.TYPE = TYPE
-        self.ID = ID
-        self.first = first
-        self.second = second
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        #return pal + self.TYPE + '\n' + pal + self.ID + '\n' + self.first.toString(i+1) + self.second.toString(i+1)
-        return pal + 'FUNDEF' + '\n' + pal + '| ' + self.ID + '\n' + '| ' + 'RET ' + self.TYPE + '\n' + self.first.toString(i+1) + self.second.toString(i+1)
-
-class Expression(object):
-    def __init__(self, expr1, sign, expr2):
-        self.expr1 = expr1
-        self.sign = sign
-        self.expr2 = expr2
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + self.sign + '\n' +  self.expr1.toString(i+1) + self.expr2.toString(i+1)
-
-class Expression1(object):
-    def __init__(self, ID, second):
-        self.ID = ID
-        self.second = second
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + self.ID.upper() + '\n' + self.second.toString(i+1)
-
-class Funcall(object):
-    def __init__(self, ID, second):
-        self.ID = ID
-        self.second = second
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + 'FUNCALL\n' + pal + '| ' + self.ID + '\n' + self.second.toString(i+1)
-
-class Repeat(object):
-    def __init__(self, REPEAT, inst, UNTIL, cond):
-        self.REAPEAT = REPEAT
-        self.inst = inst
-        self.UNTIL = UNTIL
-        self.cond = cond
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + self.REAPEAT + '\n' + self.inst.toString(i+1) + pal + self.UNTIL + '\n' + self.cond.toString(i+1)
-
-class While(object):
-    def __init__(self, WHILE, condition, instruction):
-        self.WHILE = WHILE
-        self.condition = condition
-        self.instruction = instruction
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + self.WHILE.upper() + '\n' + self.condition.toString(i+1) + self.instruction.toString(i+1)
-
-class Choice(object):
-    def __init__(self, IF, cond, inst, ELSE, inst2):
-        self.IF = IF
-        self.cond = cond
-        self.inst = inst
-        self.ELSE = ELSE
-        self.inst2 = inst2
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + self.IF.upper() + '\n' + self.cond.toString(i+1) + self.inst.toString(i+1) + pal + self.ELSE.upper() + '\n' + self.inst2.toString(i+1)
-
-class Choice2(object):
-    def __init__(self, IF, cond, instr):
-        self.IF = IF
-        self.cond = cond
-        self.instr = instr
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + self.IF.upper() + '\n' + self.cond.toString(i+1) + self.instr.toString(i+1)
-
-class Assignment(object):
-    def __init__(self, ID, expr):
-        self.ID = ID
-        self.expr = expr
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + '=' + '\n' + pal + '| ' + self.ID + '\n' + self.expr.toString(i+1)
-
-class Declaration(object):
-    def __init__(self, inits):
-        self.inits = inits
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return pal + 'DECL' + '\n' + self.inits.toString(i+1) if("toString" in dir(self.inits)) else '<<ERROR HERE>>\n'
-
-class Empty(object):
-    def __init__(self):
-        self.name = ""
-    def toString(self, i):
-        pal = ""
-        for x in range(0, i):
-            pal += '| '
-        return self.name
-
+# ...
 
--- Cparser.py
+++ Cparser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/zpython
 
 import AST
 from scanner import Scanner
@@ -48,11 +48,16 @@ class Cparser(object):
 
     def p_program(self, p):
         """program : declarations fundefs instructions"""
-        p[0] = AST.Three(p[1], p[2], p[3])
+        p[0] = AST.Node(p[1], p[2], p[3])
+        p[1].setParent(p[0])
+        p[2].setParent(p[0])
+        p[3].setParent(p[0])
 
     def p_declarations(self, p):
         """declarations : declarations declaration"""
-        p[0] = AST.Two(p[1], p[2])
+        p[0] = AST.Declarations(p[1], p[2])
+        p[1].setParent(p[0])
+        p[2].setParent(p[0])
 
     def p_declarations_1(self, p):
         """declarations : """
@@ -61,31 +66,39 @@ class Cparser(object):
     def p_declaration(self, p):
         """declaration : TYPE inits ';' """
         p[0] = AST.Declaration(p[2])
-
+        p[2].setParent(p[0])
 
     def p_declaration_1(self, p):
         """declaration : error ';' """
         p[0] = AST.Declaration(p[1])
+        p[1].setParent(p[0])
 
     def p_inits(self, p):
         """inits : inits ',' init"""
-        p[0] = AST.Two(p[1], p[3])
+        p[0] = AST.Inits(p[1], p[3])
+        p[1].setParent(p[0])
+        p[3].setParent(p[0])
 
     def p_inits_1(self, p):
         """inits : init"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.Inits1(p[1])
+        p[1].setParent(p[0])
 
     def p_init(self, p):
         """init : ID '=' expression """
         p[0] = AST.Assignment(p[1], p[3])
+        p[3].setParent(p[0])
 
     def p_instructions(self, p):
         """instructions : instructions instruction"""
-        p[0] = AST.Two(p[1], p[2])
+        p[0] = AST.Instructions(p[1], p[2])
+        p[1].setParent(p[0])
+        p[2].setParent(p[0])
 
     def p_instructions_1(self, p):
         """instructions : instruction"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.Instuctions1(p[1])
+        p[1].setParent(p[0])
 
     def p_instruction(self, p):
         """instruction : print_instr
@@ -98,43 +111,58 @@ class Cparser(object):
                        | break_instr
                        | continue_instr
                        | compound_instr"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.Instruction(p[1])
+        p[1].setParent(p[0])
 
     def p_print_instr(self, p):
         """print_instr : PRINT expression ';'
                        | PRINT error ';' """
         p[0] = AST.Expression1(p[1], p[2])
+        p[2].setParent(p[0])
 
     def p_labeled_instr(self, p):
         """labeled_instr : ID ':' instruction """
         p[0] = AST.Expression1(p[1], p[3])
+        p[1].setParent(p[0])
+        p[3].setParent(p[0])
 
     def p_assignment(self, p):
         """assignment : ID '=' expression ';' """
         p[0] = AST.Assignment(p[1], p[3])
+        p[3].setParent(p[0])
 
     def p_choice_instr(self, p):
         """choice_instr : IF '(' condition ')' instruction  %prec IFX
                         | IF '(' error ')' instruction  %prec IFX"""
         p[0] = AST.Choice2(p[1], p[3], p[5])
+        p[3].setParent(p[0])
+        p[5].setParent(p[0])
 
     def p_choice_instr_1(self, p):
         """choice_instr : IF '(' condition ')' instruction ELSE instruction
                         | IF '(' error ')' instruction ELSE instruction """
         p[0] = AST.Choice(p[1], p[3], p[5], p[6], p[7])
+        p[3].setParent(p[0])
+        p[5].setParent(p[0])
+        p[7].setParent(p[0])
 
     def p_while_instr(self, p):
         """while_instr : WHILE '(' condition ')' instruction
                        | WHILE '(' error ')' instruction """
         p[0] = AST.While(p[1], p[3], p[5])
+        p[3].setParent(p[0])
+        p[5].setParent(p[0])
 
     def p_repeat_instr(self, p):
         """repeat_instr : REPEAT instructions UNTIL condition ';' """
         p[0] = AST.Repeat(p[1], p[2], p[3], p[4])
+        p[2].setParent(p[0])
+        p[4].setParent(p[0])
 
     def p_return_instr(self, p):
         """return_instr : RETURN expression ';' """
         p[0] = AST.Expression1(p[1], p[2])
+        p[2].setParent(p[0])
 
     def p_continue_instr(self, p):
         """continue_instr : CONTINUE ';' """
@@ -146,11 +174,14 @@ class Cparser(object):
 
     def p_compound_instr(self, p):
         """compound_instr : '{' declarations instructions '}' """
-        p[0] = AST.Two(p[2], p[3])
+        p[0] = AST.CompoundInst(p[2], p[3])
+        p[2].setParent(p[0])
+        p[3].setParent(p[0])
 
     def p_condition(self, p):
         """condition : expression"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.Condition(p[1])
+        p[1].setParent(p[0])
 
     def p_const(self, p):
         """const : INTEGER
@@ -164,15 +195,20 @@ class Cparser(object):
                       | const"""
         p[0] = AST.Const(p[1])
 
-    def p_expression(self, p):
+    def p_math_expression(self, p):
         """expression : expression '+' expression
                       | expression '-' expression
                       | expression '*' expression
                       | expression '/' expression
                       | expression '%' expression
-                      | expression '|' expression
+                      | expression '^' expression"""
+        p[0] = AST.MathExpression(p[1], p[2], p[3])
+        p[1].setParent(p[0])
+        p[3].setParent(p[0])
+
+    def p_expression(self, p):
+        """expression : expression '|' expression
                       | expression '&' expression
-                      | expression '^' expression
                       | expression AND expression
                       | expression OR expression
                       | expression SHL expression
@@ -184,21 +220,26 @@ class Cparser(object):
                       | expression LE expression
                       | expression GE expression """
         p[0] = AST.Expression(p[1], p[2], p[3])
+        p[1].setParent(p[0])
+        p[3].setParent(p[0])
 
     def p_expression_1(self, p):
         """expression : ID '(' expr_list_or_empty ')'
                       | ID '(' error ')' """
         p[0] = AST.Funcall(p[1], p[3])
+        p[3].setParent(p[0])
 
 
     def p_expression_2(selfself, p):
         """expression : '(' expression ')'
                       | '(' error ')'"""
-        p[0] = AST.One(p[2])
+        p[0] = AST.Expression2(p[2])
+        p[2].setParent(p[0])
 
     def p_expr_list_or_empty(self, p):
         """expr_list_or_empty : expr_list"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.ExprList(p[1])
+        p[1].setParent(p[0])
 
     def p_expr_list_or_empty_1(self, p):
         """expr_list_or_empty : """
@@ -206,15 +247,20 @@ class Cparser(object):
 
     def p_expr_list(self, p):
         """expr_list : expr_list ',' expression"""
-        p[0] = AST.Two(p[1], p[3])
+        p[0] = AST.ExpressionList(p[1], p[3])
+        p[1].setParent(p[0])
+        p[3].setParent(p[0])
 
     def p_expr_list_1(self, p):
         """expr_list : expression"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.ExpressionList1(p[1])
+        p[1].setParent(p[0])
 
     def p_fundefs(self, p):
         """fundefs : fundef fundefs"""
-        p[0] = AST.Two(p[1], p[2])
+        p[0] = AST.FunDefs(p[1], p[2])
+        p[1].setParent(p[0])
+        p[2].setParent(p[0])
 
     def p_fundefs_1(self, p):
         """fundefs : """
@@ -223,10 +269,15 @@ class Cparser(object):
     def p_fundef(self, p):
         """fundef : TYPE ID '(' args_list_or_empty ')' compound_instr """
         p[0] = AST.Fundef(p[1], p[2], p[4], p[6])
+        #p[1].setParent(p[0])
+        #p[2].setParent(p[0])
+        p[4].setParent(p[0])
+        p[6].setParent(p[0])
 
     def p_args_list_or_empty(self, p):
         """args_list_or_empty : args_list"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.ArgListOrEmpty(p[1])
+        p[1].setParent(p[0])
 
     def p_arg_list_or_empty_1(self, p):
         """args_list_or_empty : """
@@ -234,15 +285,20 @@ class Cparser(object):
 
     def p_args_list(self, p):
         """args_list : args_list ',' arg"""
-        p[0] = AST.Two(p[1], p[3])
+        p[0] = AST.ArgList(p[1], p[3])
+        p[1].setParent(p[0])
+        p[3].setParent(p[0])
 
     def p_arg_list_1(self, p):
         """args_list : arg"""
-        p[0] = AST.One(p[1])
+        p[0] = AST.ArgList1(p[1])
+        p[1].setParent(p[0])
 
     def p_arg(self, p):
         """arg : TYPE ID """
         p[0] = AST.Arg(p[1], p[2])
+        #p[1].setParent(p[0])
+        #p[2].setParent(p[0])
 
 
 
